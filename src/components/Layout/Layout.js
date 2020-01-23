1389ba0cf141201c196e451231902a93d73bb31d
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2020-01-23 09:34:53 +0300

    Add new components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Toolbar from '../navigation/Toolbar/Toolbar';
+
 import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={ classes.Content }>
             { props.children }
         </main>
